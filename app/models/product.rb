f69a5e9fad0294c901c23c9df111f4a2f9fc3fe7
Dev-Sanin <sanin.redzepovic@gmail.com>
2018-08-15 23:54:41 +0200

    changed all erb to slim files, added store functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
     validates :title, :description, :image_url, presence: true
-    validates :title, uniqueness: true
+    validates :title, uniqueness: true, length: { 
+                minimum: 10, 
+                message: 'You have to insert more than 10 characters!' 
+            }
     validates :image_url, allow_blank: true, format: {
                 with: %r{\.(gif|jpg|png)\Z}i,
                 message: 'must be a URL for GIF, JPG or PNG image.'
               }
-    validates :price, numericality: { greater_than_or_equal_to: 0.01}
+    validates :price, numericality: { greater_than_or_equal_to: 0.01}   
 end
