bcd99e0d48b3947d4feb81fc6ce0b8179d473b08
Dev-Sanin <sanin.redzepovic@gmail.com>
2018-08-17 12:51:47 +0200

    ajax implemented on Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -61,7 +62,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to cart_path(cart) }
+      format.html { redirect_to store_index_url }
       format.json { head :no_content }
     end
   end
